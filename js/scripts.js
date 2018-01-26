dac69d4ac85620c0a674179610cc24e17f7be2a7
ivanramos148248@gmail.com <ivanramos148248@gmail.com>
2018-01-26 12:03:51 -0800

    add README.md and js change

--- js/scripts.js
+++ js/scripts.js
@@ -17,6 +17,8 @@ function pizzaOrder() {
 }
 
     Pizza.prototype.fullPizza = function() {
+      if (this.size === "Large") {
+        $("#hello").show();
 
     }
 
@@ -36,43 +38,3 @@ $(document).ready(function(){
 
   });
 });
-
-function Ticket(movie, time, age, status) {
-  this.movie = movie;
-  this.time = time;
-  this.age = age;
-  this.status = status;
-}
-
-/*function ticketMaker() {
-  var movie = $("#movie").val();
-  var time = $("#time").val();
-  var age = $("#age").val();
-  var status = $("#status").val();
-  var newTicket = new Ticket(movie, time, age, status)
-
-  return newTicket;
-}
-
-Ticket.prototype.price = function(){
-  var cost = 0;
-  if (this.movie === "Lady Bird") {
-    cost = 15;
-  } else {
-    cost = 10;
-  }
-  if (this.time === "11:45 AM" || this.time === "2:30 PM") {
-    cost = cost - 2;
-  } else if(this.time === "6:30 PM" || this.time === "7:00 PM"){
-    cost = cost + 1;
-  }
-  if (this.age === "56-80" ) {
-    cost = cost -2;
-  }else if(this.age === "81+"){
-    return 0;
-  }
-  if (this.status === "None") {
-    return cost;
-  }
-  return cost - 1;
-}
