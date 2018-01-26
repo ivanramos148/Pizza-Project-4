5869dd038d62a390912be5bfe5d9df8bd4d1b3a4
ivanramos148248@gmail.com <ivanramos148248@gmail.com>
2018-01-26 15:07:03 -0800

    add inputs and var

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,48 @@
-function Pizza(size, toppings) {
+//  Business Logic
+function Pizza(size, toppings, extras, sodas) {
     this.size = size;
     this.toppings = toppings;
-
+    this.extras = extras;
+    this.sodas = sodas;
 }
-
 function chosenPizza() {
   var size = $("#size").val();
   var toppings = $("#toppings").val();
-  var newCost = new Pizza(size, toppings);
+  var extras = $("#extras").val();
+  var sodas = $("#sodas").val();
+  var newCost = new Pizza(size, toppings, extras, sodas);
 
   return newCost
 }
 
 
-Pizza.prototype.cost = function(){
+Pizza.prototype.cost = function() {
   var cost = 0;
- if (this.size === "small" || this.size === "medium"){
-  cost = 10;
+  if (this.size === 'medium' || this.size === 'small') {
+      cost = cost + 5;
   } else {
-    cost = 20;
-  }
-if (this.toppings === "cheese" || this.toppings === "peperoni" || this.toppings === "artichoke"|| this.toppings === "anchovy"){
-    cost = cost + 1
-} else {
-  cost = 0
-}
+      cost = cost + 8;
+  };
+  if (this.toppings === 'peperoni' || this.toppings === 'artichoke') {
+      cost = cost + 2;
+  } else {
+      cost = cost + 0;
+  };
+  if (this.extras === 'Cheese' || this.extras === 'Dessert') {
+      cost = cost + 3;
+  } else {
+      cost = cost + 0;
+  };
+  if (this.sodas === 'sprite' || this.sodas === 'coke') {
+      cost = cost + 5;
+  } else {
+      cost = cost + 0;
+  };
+
   return cost;
+
 }
+//User Interface
 $(document).ready(function(){
   $("#pizza").submit(function(event){
     event.preventDefault();
