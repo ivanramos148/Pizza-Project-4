12231e634204f54201fa35e8c5cac65886c74fe7
ivanramos148248@gmail.com <ivanramos148248@gmail.com>
2018-01-26 10:01:45 -0800

    add constructs to js business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,15 @@
 //Business Logic
+function Pizza(size, toppings, sodas) {
+  this.size = size
+  this.toppings = toppings
+  this.sodas = sodas
+}
 
+    Pizza.prototype.fullPizza() {
+
+    }
+
+    var
 
 
 
